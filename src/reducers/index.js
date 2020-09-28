2b42252dbd43b73ef3233354a79a4fda07f1f5e7
David Slusser <dslusser13@gmail.com>
2020-09-28 16:55:05 -0700

    q

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
-export default combineReducers({ dummy: () => "hi" });
+export default combineReducers({ postsReducer });
